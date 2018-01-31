f220ef4eff623453807f3474d0c242a7cd979600
mishaszu <mishaszu@gmail.com>
2018-01-31 21:16:18 +0100

    set background colors

--- src/colors/pallete.js
+++ src/colors/pallete.js
@@ -1,13 +1,23 @@
 module.exports = {
   pallete: {
     black: "\x1b[30m",
-    red: "\x1b[31m",
-    green: "\x1b[32m",
-    yellow: "\x1b[33m",
     blue: "\x1b[34m",
-    magneta: "\x1b[35m",
     cyan: "\x1b[36m",
+    green: "\x1b[32m",
+    magneta: "\x1b[35m",
     white: "\x1b[37m",
-    reset: '\x1b[0m'
+    red: "\x1b[31m",
+    reset: '\x1b[0m',
+    yellow: "\x1b[33m",
+    bg: {
+      black: "\x1b[40m",
+      blue: "\x1b[44m",
+      cyan: "\x1b[46m",
+      green: "\x1b[42m",
+      magneta: "\x1b[45m",
+      red: "\x1b[41m",
+      white: "\x1b[47m",
+      yellow: "\x1b[43m",
+    },
   }
 }
