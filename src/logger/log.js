fc851089fa06bb6ce504de9805e074d3dd4c972d
mishaszu <mishaszu@gmail.com>
2018-01-31 20:28:08 +0100

    add colors and tests

--- src/logger/log.js
+++ src/logger/log.js
@@ -1,5 +1,8 @@
 module.exports = {
   log: function() {
+    if (!this.value) {
+      this.value = ''
+    }
     if (this.color) {
       console.log(this.color, this.dateString, this.value, this.pallete.reset)
     } else {
--- src/pad/lvl.js
+++ src/pad/lvl.js
@@ -1,33 +1,33 @@
 module.exports = {
   lvl1: function(string){
     if (string) {
-      this.value = this.value.padStart(5, string)
-    } else {
-      this.value = this.value.padStart(5)
-    }
-    return this
-  },
-  lvl2: function(string){
-    if (string) {
       this.value = this.value.padStart(10, string)
     } else {
       this.value = this.value.padStart(10)
     }
     return this
   },
+  lvl2: function(string){
+    if (string) {
+      this.value = this.value.padStart(20, string)
+    } else {
+      this.value = this.value.padStart(20)
+    }
+    return this
+  },
   lvl3: function(string){
     if (string) {
-      this.value = this.value.padStart(15, string)
+      this.value = this.value.padStart(30, string)
     } else {
-      this.value = this.value.padStart(15)
+      this.value = this.value.padStart(30)
     }
     return this
   },
   lvl4: function(string){
     if (string) {
-      this.value = this.value.padStart(20, string)
+      this.value = this.value.padStart(40, string)
     } else {
-      this.value = this.value.padStart(20)
+      this.value = this.value.padStart(40)
     }
     return this
   },
