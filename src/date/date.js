6997d87f9101b7cdaa87d8a7fcc685d6f8f99ddb
mishaszu <mishaszu@gmail.com>
2018-02-02 16:27:51 +0100

    updated descriptive text and include add zero to numbers < 10

--- src/date/date.js
+++ src/date/date.js
@@ -1,11 +1,13 @@
+const {utils} = require('../utils')
+
 module.exports = {
   dateString: '',
   date: function(){
     const s = {}
     const now = new Date()
-    s.hour = now.getHours()
-    s.minutes = now.getMinutes()
-    s.seconds = now.getSeconds()
+    s.hour = utils.addZero(now.getHours())
+    s.minutes = utils.addZero(now.getMinutes())
+    s.seconds = utils.addZero(now.getSeconds())
     this.dateString = `[${s.hour}:${s.minutes}:${s.seconds}]`
     return this
   }
--- src/logger/log.js
+++ src/logger/log.js
@@ -1,19 +1,26 @@
+"strict mode"
+
 module.exports = {
   log: function() {
+    let descriptiveText = ''
+    if (this.descriptiveText) {
+      descriptiveText = `${this.descriptiveTextBrackets[0]}${this.descriptiveText}${this.descriptiveTextBrackets[1]}`
+    }
     if (!this.value) {
       this.value = ''
     }
+
     if (this.color || this.bgColor || this.effect) {
       console.log(
         `${this.color}${this.bgColor}${this.effect}`,
-        `${this.dateString}${this.descriptiveTextBrackets[0]}${this.descriptiveText}${this.descriptiveTextBrackets[1]}`,
+        `${this.dateString}${descriptiveText}`,
         this.value,
         this.pallete.reset
       )
     } else {
       console.log(
         this.pallete.reset,
-        `${this.dateString}${this.descriptiveTextBrackets[0]}${this.descriptiveText}${this.descriptiveTextBrackets[1]}`,
+        `${this.dateString}${descriptiveText}`,
         this.value
       )
     }
